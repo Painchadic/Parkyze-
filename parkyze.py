1a879a850192b761a8efd5e71bf0e8f5efbf9c7d
paincharth <paincharth@cy-tech.fr>
2022-11-24 08:43:39 +0100

    Changement de réunion

--- parkyze.py
+++ parkyze.py
@@ -3,17 +3,43 @@ import math as m
 import parkyzeClass as pc
 import numpy as np
 import shapely.geometry
+import random
 
-espace = pc.EspaceDeTravail([(-20,-32), (-20,20), (30,20), (30,-32), (-20,-32)])
-parking = [pc.Rampe(20, 2, [-25,0], 0, 2)]
-parking += [pc.Route(parking[0], 12, 2, 0)]
-parking += [pc.Route(parking[1], 12, 2, 0)]
-for i in range(3):
-    parking += [pc.Route(parking[i], 31, 2, -parking[i].angle - m.pi/2)]
+espace = pc.EspaceDeTravail([(-20,-32), (-20,20), (0,25), (30,20), (30,-32), (-20,-32)])
+listAngle = [0, m.pi/2, -m.pi/2]
 
-parking += [pc.Noyaux(parking, [(10,10), (10,15), (20,15), (20,10), (10,10)])]
-parking += pc.remplissagePlace(parking, 5, 2.5)
+def autoRoute(epaisseur, parking, nbRoutes):
+    listAvailable = [(0, i) for i in range(3)]
 
+    return autoRouteAux(epaisseur, parking, listAvailable, nbRoutes)
+
+def autoRouteAux(epaisseur, parking, listAvailable, nbRoutes):
+    #listAvailable de la forme [(0, 0), (0, 1)]
+    if nbRoutes < 1:
+        return parking
+    longueur = len(listAvailable)
+    index = random.randint(0, longueur - 1)
+    route, angle = listAvailable[index]
+    print(nbRoutes)
+    route = parking[route]
+    angle = listAngle[angle]
+    a = pc.Route(route, random.random() * 20 + 5, epaisseur, angle, espace)
+    if a.valide :
+        parking += [a]
+        nbRoutes -= 1
+        listAvailable.pop(index)
+        listAvailable += [(len(parking) - 1, i) for i in range(3)]
+        return autoRouteAux(epaisseur, parking, listAvailable, nbRoutes)
+    return autoRouteAux(epaisseur, parking, listAvailable, nbRoutes)
+
+
+
+parking = [pc.Rampe(20, 5, [-25,0], 0, 2)]
+#parking += [pc.Noyaux(parking, [(10,10), (10,15), (20,15), (20,10), (10,10)])]
+parking = autoRoute(5, parking, 10)
+
+
+parking = pc.remplissagePlace(parking, 5, 2.5, espace)
 
 
 poly = []
